b56da44b13b6807f6feec5724c0995ce606b23f2
Arnold Wainaina <arnoldwainaina2865@gmail.com>
2025-04-13 20:19:59 +0300

    expense tracker

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,7 +1,8 @@
-// src/App.jsx
 import { useState } from 'react';
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseTable from './components/ExpenseTable';
+import './App.css';
+
 
 function App() {
   const [expenses, setExpenses] = useState([]);
